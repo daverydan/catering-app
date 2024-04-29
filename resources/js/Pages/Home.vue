fccca0a11946d7e175e494933d55263ae6064bc9
daverydan <danny.w.avery@gmail.com>
2024-04-29 19:47:16 -0400

    Add Vue 3 & Inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+function alertMe() {
+    alert('Alert!!!');
+}
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <h1>Home Page</h1>
+
+    <button type="button" @click="alertMe">Alert Me</button>
+</template>
